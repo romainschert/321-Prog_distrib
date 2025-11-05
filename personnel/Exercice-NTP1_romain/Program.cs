3327f21ab3e3bdee0447c8b579e40145c4f42a1b
romainschert <150144520+romainschert@users.noreply.github.com>
2025-11-05 11:22:38 +0100

    Etape E et F exercice NPM 1

--- personnel/Exercice-NTP1_romain/Program.cs
+++ personnel/Exercice-NTP1_romain/Program.cs
@@ -9,13 +9,19 @@ namespace ntp
     {
         static void Main(string[] args)
         {
-            string ntpServer = "0.ch.pool.ntp.org";  // 0.ch .... = nom de domaine d'un serveur NTP publique
+            string[] ntpServer = {
+                "0.pool.ntp.org",
+                "1.pool.ntp.org",
+                "time.google.com",
+                "time.cloudflare.com",
+                "0.ch.pool.ntp.org"
+            };  // 0.ch .... = nom de domaine d'un serveur NTP publique
 
             byte[] timeMessage = new byte[48];// variable de type byte[]
 
             timeMessage[0] = 0x1B;// 0x1B type de requete eenvoyer au serveur
 
-            IPEndPoint ntpReference = new IPEndPoint(Dns.GetHostAddresses(ntpServer)[0], 123);
+            IPEndPoint ntpReference = new IPEndPoint(Dns.GetHostAddresses(ntpServer[4])[0], 123);
 
             UdpClient client = new UdpClient();
             client.Connect(ntpReference);
@@ -35,9 +41,39 @@ namespace ntp
             TimeSpan Difftime = DateTime.UtcNow - ntpTime;
             Console.WriteLine(Difftime.TotalSeconds);
 
-            ntpTime = DateTime.UtcNow;
-            Console.WriteLine(ntpTime);
+            DateTime localTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.Local);
+            Console.WriteLine($"Heure locale : {localTime}");
+
+        
+            TimeZoneInfo swissTimeZone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+            DateTime swissTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, swissTimeZone);
+            Console.WriteLine($"Heure suisse : {swissTime}");
+
+            TimeZoneInfo utcTimeZone = TimeZoneInfo.Utc;
+            DateTime backToUtc = TimeZoneInfo.ConvertTime(localTime, TimeZoneInfo.Local, utcTimeZone);
+            Console.WriteLine($"Retour vers UTC : {backToUtc}");
+
+            DateTime i = DateTime.UtcNow;
+            DisplayWorldClocks(i);
         }
+        public static void DisplayWorldClocks(DateTime utcTime)
+        {
+            var timeZones = new[]
+            {
+                ("UTC", TimeZoneInfo.Utc),
+                ("New York", TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time")),
+                ("London", TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time")),
+                ("Tokyo", TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time")),
+                ("Sydney", TimeZoneInfo.FindSystemTimeZoneById("AUS Eastern Standard Time"))
+            };
+
+            foreach (var (name, tz) in timeZones)
+            {
+                var localTime = TimeZoneInfo.ConvertTimeFromUtc(utcTime, tz);
+                Console.WriteLine($"{name}: {localTime:yyyy-MM-dd HH:mm:ss}");
+            }
+        }
+
     }
 
     static class NtpPacket
@@ -52,5 +88,9 @@ namespace ntp
 
             return networkDateTime;
         }   
-    }   
+    }
+    
+
+
+
 }
